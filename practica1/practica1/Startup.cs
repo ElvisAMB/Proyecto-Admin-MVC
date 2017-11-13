7122660db5c5cabe42d06743432dc181946e7704
ELVISAMB <elvis.mora@confianza.com.ec>
2017-11-13 15:52:56 -0500

    Se procede abrir desde SistemasDes

---
+++ practica1/practica1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practica1.Startup))]
+namespace practica1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
