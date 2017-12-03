1df64480ba7d87a469bf31c7e6eff901f0430dff
Elvis A. Mora B. <pegazuz@live.com>
2017-12-03 15:41:10 -0500

    Se implementó el jqgrid para presentar los datos del agenda

---
+++ practica1/Controllers/AgendaTelefonicaController.cs
@@ -0,0 +1,105 @@
+﻿using Dominio.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace practica1.Controllers
+{
+  public class AgendaTelefonicaController : Controller
+  {
+    // GET: AgendaTelefonica
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public JsonResult GetValues(string sidx, string sord, int page, int rows)  //Gets the todo Lists.
+    {
+      var _listadoAgenda = new Agenda().ConsultarAgenda();
+      int pageIndex = Convert.ToInt32(page) - 1;
+      int pageSize = rows;
+      var Results = _listadoAgenda.Select(
+              a => new
+              {
+                a.Id,
+                a.FirstName,
+                a.LastName,
+                a.Email,
+                a.Extension,
+                a.Estado
+              });
+      int totalRecords = Results.Count();
+      var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+      if (sord.ToUpper() == "DESC")
+      {
+        Results = Results.OrderByDescending(s => s.Id);
+        Results = Results.Skip(pageIndex * pageSize).Take(pageSize);
+      }
+      else
+      {
+        Results = Results.OrderBy(s => s.Id);
+        Results = Results.Skip(pageIndex * pageSize).Take(pageSize);
+      }
+      var jsonData = new
+      {
+        total = totalPages,
+        page,
+        records = totalRecords,
+        rows = Results
+      };
+      return Json(jsonData, JsonRequestBehavior.AllowGet);
+    }
+
+    // TODO:insert a new row to the grid logic here
+    [HttpPost]
+    public string Create([Bind(Exclude = "Id")] Agenda obj)
+    {
+      string msg;
+      try
+      {
+        if (ModelState.IsValid)
+        {
+          obj.Guardar();
+          msg = "Saved Successfully";
+        }
+        else
+        {
+          msg = "Validation data not successfull";
+        }
+      }
+      catch (Exception ex)
+      {
+        msg = "Error occured:" + ex.Message;
+      }
+      return msg;
+    }
+    public string Edit(Agenda obj)
+    {
+      string msg;
+      try
+      {
+        if (ModelState.IsValid)
+        {
+          obj.Guardar();
+          msg = "Saved Successfully";
+        }
+        else
+        {
+          msg = "Validation data not successfull";
+        }
+      }
+      catch (Exception ex)
+      {
+        msg = "Error occured:" + ex.Message;
+      }
+      return msg;
+    }
+    public string Delete(int Id)
+    {
+      var objFind = new Agenda().ConsultarAgendaPersona(Id);
+      objFind.Estado = false;
+      objFind.Guardar();
+      return "Deleted successfully";
+    }
+  }
+}
---
+++ practica1/Scripts/appjs/myjqgrid.js
@@ -0,0 +1,79 @@
+﻿$(function () {
+  debugger;
+  $("#grid").jqGrid({
+    url: "/AgendaTelefonica/GetValues",
+    datatype: 'json',
+    mtype: 'Get',
+    //table header name 
+    colNames: ['Id', 'Nombres', 'Apellidos', 'Correo', 'Extensión', 'Estado'],
+    //colModel takes the data from controller and binds to grid 
+    colModel: [
+        { key: true, hidden: true, name: 'Id', index: 'Id', editable: true },
+        { key: false, name: 'FirstName', index: 'FirstName', editable: true },
+        { key: false, name: 'LastName', index: 'LastName', editable: true },
+        { key: false, name: 'Email', index: 'Email', editable: true },
+        { key: false, name: 'Extension', index: 'Extension', editable: true },
+        { key: false, name: 'Estado', index: 'Estado', editable: true }
+        //{ key: false, name: 'DOB', index: 'DOB', editable: true, formatter: 'date', formatoptions: { newformat: 'd/m/Y' } }
+    ],
+
+    pager: jQuery('#pager'),
+    rowNum: 10,
+    rowList: [10, 20, 30, 40],
+    height: '100%',
+    viewrecords: true,
+    caption: 'Teléfonos',
+    emptyrecords: 'No records to display',
+    jsonReader: {
+      root: "rows",
+      page: "page",
+      total: "total",
+      records: "records",
+      repeatitems: false,
+      Id: "0"
+    },
+    autowidth: true,
+    multiselect: false
+    //pager-you have to choose here what icons should appear at the bottom
+    //like edit,create,delete icons
+  }).navGrid('#pager', { edit: true, add: true, del: true, search: false, refresh: true },
+      {
+        // edit options
+        zIndex: 100,
+        url: '/AgendaTelefonica/Edit',
+        closeOnEscape: true,
+        closeAfterEdit: true,
+        recreateForm: true,
+        afterComplete: function (response) {
+          if (response.responseText) {
+            alert(response.responseText);
+          }
+        }
+      },
+      {
+        // add options
+        zIndex: 100,
+        url: "/AgendaTelefonica/Create",
+        closeOnEscape: true,
+        closeAfterAdd: true,
+        afterComplete: function (response) {
+          if (response.responseText) {
+            alert(response.responseText);
+          }
+        }
+      },
+      {
+        // delete options
+        zIndex: 100,
+        url: "/AgendaTelefonica/Delete",
+        closeOnEscape: true,
+        closeAfterDelete: true,
+        recreateForm: true,
+        msg: "Are you sure you want to delete this task?",
+        afterComplete: function (response) {
+          if (response.responseText) {
+            alert(response.responseText);
+          }
+        }
+      });
+});
